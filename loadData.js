815cda19ceb738b0bcfb9cb6dfe0574a08d3a385
saki komai <55061969+saki789@users.noreply.github.com>
2023-09-03 11:59:02 +1200

    Update loadData.js

--- loadData.js
+++ loadData.js
@@ -1,23 +1,14 @@
-// Function to populate the table with JSON data
-function populateTable(data) {
-  const tbody = document.querySelector('table tbody');
-  
-  data.data.forEach(item => {
-    const row = document.createElement('tr');
-    
-    // Create table cells for each data property
-    const columns = ['siteName', 'guardName', 'hoursIn', 'hoursOut', 'n', 't', 'd', 'total', 'extra', 'grandTotal'];
-    columns.forEach(columnName => {
-      const cell = document.createElement('td');
-      cell.textContent = item[columnName];
-      row.appendChild(cell);
-    });
-    
-    tbody.appendChild(row);
-  });
+// Function to load JSON data from a URL and populate the table
+function loadJSONAndPopulateTable() {
+  fetch('https://saki789.github.io/Table/data.json')
+    .then(response => response.json())
+    .then(data => {
+      populateTable(data);
+    })
+    .catch(error => console.error('Error loading JSON:', error));
 }
 
 // Load JSON data and populate the table when the page is ready
 document.addEventListener('DOMContentLoaded', () => {
-  loadJSON(populateTable);
+  loadJSONAndPopulateTable();
 });
