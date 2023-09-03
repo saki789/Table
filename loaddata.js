5756f27553c1e55e47cdc514f3725f263c9574ad
saki komai <55061969+saki789@users.noreply.github.com>
2023-09-03 20:14:07 +1200

    Update loaddata.js

--- loaddata.js
+++ loaddata.js
@@ -4,19 +4,22 @@ function populateTable(data) {
     data.forEach(item => {
         const row = document.createElement('tr');
 
-        const columns = ['Site', 'Guards', 'Time.In', 'Time.Out', 'N', 'T', 'D', 'Total', 'Extra', 'Grand Total'];
+        const columns = ['Site', 'Guards', ['Time.In', 'Time.Out'], 'N', 'T', 'D', 'Total', 'Extra', 'Grand Total'];
 
         columns.forEach(column => {
-            const cell = document.createElement('td');
-            const columnNames = column.split('.'); // Split nested column names
-            let currentData = item;
-
-            columnNames.forEach(colName => {
-                currentData = currentData[colName];
-            });
-
-            cell.textContent = currentData;
-            row.appendChild(cell);
+            if (Array.isArray(column)) {
+                const subColumnGroup = document.createElement('td');
+                column.forEach(subColumnName => {
+                    const subColumnCell = document.createElement('div');
+                    subColumnCell.textContent = item[subColumnName];
+                    subColumnGroup.appendChild(subColumnCell);
+                });
+                row.appendChild(subColumnGroup);
+            } else {
+                const cell = document.createElement('td');
+                cell.textContent = item[column];
+                row.appendChild(cell);
+            }
         });
 
         tbody.appendChild(row);
